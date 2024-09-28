2fb574cc1a2dd677a0ed7ba6b8ce5aad7f5ea503
Argoty <leonardoargotydev@gmail.com>
2024-09-28 15:56:34 -0500

    Primer commit prueba

---
+++ src/main/java/Models/Producto.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Models;
+
+/**
+ *
+ * @author Javier Argoty, Simon Cruz y Luis Sinisterra 
+ */
+public class Producto {
+    
+}
