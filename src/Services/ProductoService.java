c42d4e9b979b6520bf169d47c2731afad86bcb8c
Luis Eduardo Sinisterra Montaño <luiseduardosiniterramontano@gmail.com>
2024-09-28 21:18:12 -0500

    Prueba

---
+++ src/Services/ProductoService.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Services;
+
+/**
+ *
+ * @author luis
+ */
+public class ProductoService {
+   
+    public static void main(String[] args) {
+        System.out.println("Hola desde los servicios de productos");
+    }
+             
+}
