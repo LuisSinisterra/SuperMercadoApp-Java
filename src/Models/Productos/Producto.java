ee3f19e3d6741439ae81d79d39b513817a8397c1
Luis Eduardo Sinisterra Montaño <luiseduardosiniterramontano@gmail.com>
2024-09-29 20:06:14 -0500

    Implementacion de modelo producto

--- src/Models/Productos/Producto.java
+++ src/Models/Productos/Producto.java
@@ -9,8 +9,50 @@ package Models.Productos;
  * @author Luis Sinisterra, Simon Cruz, Leonardo Argoty
  */
 public class Producto {
+    
     private int codigoProduto;
     private String nombreProducto;
     private double precio;
     private int stock;
+    
+    public void setCodigoProdcuto(int codigoProducto){
+        this.codigoProduto = codigoProducto;
+    }
+    
+    public int getCodigoProducto(){
+        return this.codigoProduto;
+    }
+    
+    public void setNombreProducto(String nombreProducto){ 
+        this.nombreProducto = nombreProducto;
+    }
+    
+    public String getNombreProducto(){
+        return this.nombreProducto;
+    }
+    
+    public void setPrecio(int precio){
+        this.precio = precio;
+    }
+    
+    public double getPrecio(){
+        return this.precio;
+    }
+    
+    public void setStock(int stock){
+        this.stock = stock;
+    }
+    
+    public int getStock(){
+        return this.stock;
+    }
+    
+    public void calcularPrecio(){
+        //Codigo de calcular precio
+    }
+    
+    public void mostrarInformacion(){
+        //Codigo de mostrar informacion
+    }
+    
 }
