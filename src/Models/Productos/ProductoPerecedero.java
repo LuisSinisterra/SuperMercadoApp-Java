e225efa4068c6b516f318f6bbdc5bddc20187474
Simón <simondavidcruzsuazo@gmail.com>
2024-09-30 08:22:25 -0500

    Implementacion base(herencia, abstracion, etc) en modelos de la clase producto y sus derivadas

--- src/Models/Productos/ProductoPerecedero.java
+++ src/Models/Productos/ProductoPerecedero.java
@@ -8,6 +8,30 @@ package Models.Productos;
  *
  * @author Luis Sinisterra, Simon Cruz, Leonardo Argoty
  */
-public class ProductoPerecedero {
-    
+public class ProductoPerecedero extends Producto{
+
+    String fechaCaducidad;
+
+    public ProductoPerecedero(int codigoProduto, String nombreProducto, double precio, int stock, String fechaCaducidad) {
+        super(codigoProduto, nombreProducto, precio, stock);
+        this.fechaCaducidad = fechaCaducidad;
+    }
+
+    @Override
+    public double calcularPrecio() {
+        return 2;//valor temporal
+    }
+
+    @Override
+    public void mostrarInformacion() {
+
+    }
+
+    public String getFechaCaducidad() {
+        return fechaCaducidad;
+    }
+
+    public void setFechaCaducidad(String fechaCaducidad) {
+        this.fechaCaducidad = fechaCaducidad;
+    }
 }
