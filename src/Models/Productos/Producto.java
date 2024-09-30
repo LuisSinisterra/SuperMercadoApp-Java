2b01ece757ee94693205b839da07d1c13aa3fbca
Luis Eduardo Sinisterra Montaño <luiseduardosiniterramontano@gmail.com>
2024-09-29 19:53:30 -0500

    Implementacion del modelo producto

--- src/Models/Productos/Producto.java
+++ src/Models/Productos/Producto.java
@@ -9,5 +9,8 @@ package Models.Productos;
  * @author Luis Sinisterra, Simon Cruz, Leonardo Argoty
  */
 public class Producto {
-    
+    private int codigoProduto;
+    private String nombreProducto;
+    private double precio;
+    private int stock;
 }
