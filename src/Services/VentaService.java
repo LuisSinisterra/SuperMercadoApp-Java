a2882d7c316d6ab9cd482f79084a5291435b7916
Simón <simondavidcruzsuazo@gmail.com>
2024-10-01 16:19:15 -0500

    desarrollo en proceso

--- src/Services/VentaService.java
+++ src/Services/VentaService.java
@@ -1,5 +1,22 @@
 package Services;
 
+import Models.Venta;
+
+import java.util.ArrayList;
+
 public class VentaService {
+    ArrayList<Venta> ventas;
+
+    public VentaService(ArrayList<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+
+    //registrar venta(excepcion stock insuficiente)
+    //version de prueba
+    public void agregarVenta(Venta venta) {
+        this.ventas.add(venta);
+    }
+
 
 }
